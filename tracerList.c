221621d019d3e3da2eec3ac6cec754db7c542f14
Chris Tiede <cwt271@nyu.edu>
2017-07-10 15:55:36 -0400

    Building out tracer and tracerList accordingly->now first initialize the list, then the tracers

--- tracerList.c
+++ tracerList.c
@@ -4,38 +4,8 @@ int getN0( int, int, int );
 
 void init_tracerList( struct domain *theDomain ){
 
-//--------Put this into a function -> give theDomain vars N0r, N0z, delR, delZ------------------
-   int Num_R = theDomain->theParList.Num_R;
-   int Num_Z = theDomain->theParList.Num_Z;
-   int phi_max = theDomain->theParList.phimax;
-   int *dim_rank = theDomain->dim_rank;
-   int *dim_size = theDomain->dim_size;
-
-   int N0r = getN0( dim_rank[0], dim_size[0], Num_R );
-   int N1r = getN0( dim_rank[0]+1, dim_size[0], Num_R);
-   int Nr = N1r - N0r;
-
-   int N0z = getN0( dim_rank[1], dim_size[1], Num_Z );
-   int N1z = getN0( dim_rank[1]+1, dim_size[1], Num_Z);
-   int Nz = N1z - N0zi;
-
-   double dr = 1.0/(double)Num_R;
-   double r0 = (double)N0r*dr;
-   double delr = Nr*dr;
-
-   double dz = 1./(double)Num_Z;
-   double z0 = (double)N0z*dz;
-   double delz = Nz*dz;
-//------------------------------------------------------------------------------
-
-   srand(theDomain->rank);
-   rand();
-
    int Ntr = theDomain->Ntr;
    struct tracerList *theList = theDomain->theTracers;
-
-   //struct tracer tr[Ntr];           //array of tracers for initializing
-   //struct tracer *temp = &tr[0];
    theList->head = NULL;
 
    int n;
@@ -51,4 +21,10 @@ void addTracer( *theList ){
    struct tracer tr = {0};
    theList->head = tr;
    tr.next = temp;
+}
+
+int getListSize( *theList ){
+
+
+  return size;
 }
