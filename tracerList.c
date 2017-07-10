6379877e989ff7c8c19912ad82be9a080a6c0f28
Chris Tiede <cwt271@ria.cosmo.fas.nyu.edu>
2017-07-10 15:13:12 -0400

    Working on building tracerList

---
+++ tracerList.c
@@ -0,0 +1,67 @@
+#include "paul.h"
+
+int getN0( int, int, int );
+
+void init_tracerList( struct domain *theDomain ){
+
+//--------Put this into a function -> give theDomain vars N0r, N0z, delR, delZ------------------  
+   int Num_R = theDomain->theParList.Num_R;
+   int Num_Z = theDomain->theParList.Num_Z;
+   int phi_max = theDomain->theParList.phimax;
+   int *dim_rank = theDomain->dim_rank;
+   int *dim_size = theDomain->dim_size;
+
+   int N0r = getN0( dim_rank[0], dim_size[0], Num_R );
+   int N1r = getN0( dim_rank[0]+1, dim_size[0], Num_R);
+   int Nr = N1r - N0r;
+
+   int N0z = getN0( dim_rank[1], dim_size[1], Num_Z );
+   int N1z = getN0( dim_rank[1]+1, dim_size[1], Num_Z);
+   int Nz = N1z - N0zi;
+
+   double dr = 1.0/(double)Num_R;
+   double r0 = (double)N0r*dr;
+   double delR = Nr*dr;
+
+   double dz = 1./(double)Num_Z;
+   double z0 = (double)N0z*dz;
+   double delZ = Nz*dz;
+//-----------Save these things in gridsetup.c------------------------------------------------
+
+   srand(theDomain->rank);
+   rand();
+
+   int Ntr = theDomain->Ntr;
+   struct tracerList *theList = theDomain->theTracers;
+
+   //struct tracer tr[Ntr];           //array of tracers for initializing
+   //struct tracer *temp = &tr[0];
+   theList->head = NULL;
+   
+   int n;
+   for( n=0; n<Ntr; ++n){     //create linked list of Ntr tracers
+      addTracer( theList );
+   } 
+
+/*   
+   int n;
+   double r, phi, z;
+   for( n=0; n<Ntr; ++n ){
+      r = r0 + ( (double)rand()/(double)RAND_MAX )*delR;
+      z = z0 + ( (double)rand()/(double)RAND_MAX )*delZ;
+      phi = ( (double)rand()/(double)RAND_MAX )*phi_max;          ------> Move this all to initTracers
+      tr[n].R = r; tr[n].Z = z; tr[n].Phi = phi;
+      tr[n].Type = 0;
+
+      tr[n].next = &tr[n+1];
+   }
+*/
+}
+
+void addTracer( *theList ){
+
+   struct tracer *temp = theList->head;
+   struct tracer tr = {0};
+   theList->head = tr;
+   tr.next = temp;
+}
