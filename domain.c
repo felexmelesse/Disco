b48835eb21b7e8f65cc82e429fcef200849e8172
Chris Tiede <cwt271@ria.cosmo.fas.nyu.edu>
2017-07-21 11:47:08 -0400

    Shock test

--- domain.c
+++ domain.c
@@ -49,12 +49,9 @@ void setupDomain( struct domain * theDomain ){
    //int Ntr = theDomain->Ntr;
    //theDomain->theTracers = (struct tracer *) malloc( Ntr*sizeof(struct tracer) );
    theDomain->theTracers = (struct tracerList *) malloc( sizeof(struct tracerList) );
-   printf("Created space for tracerList\n");
    init_tracerList( theDomain );  //initialize each processor's tracer list
-   printf("Initialized Tracer List\n");
    initializeTracers( theDomain );  //initialize tracers on each process
    //initTracers_Rand( theDomain );
-   printf("Initialized Tracers\n");
 
    int num_tools = num_diagnostics();
    theDomain->num_tools = num_tools;
