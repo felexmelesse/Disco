b21e54501b3228dfebb5f210b58698748f6d1b1e
Geoff Ryan <geoffrey.s.ryan@gmail.com>
2018-07-09 04:51:51 -0400

    Added Cartesian geometry setup. Still bugtesting.

---
+++ Geometry/cartesian.c
@@ -0,0 +1,129 @@
+
+#include "../paul.h"
+
+static double y_max = 0.0;
+
+void setGeometryParams( struct domain * theDomain ){
+    y_max = theDomain->phi_max;
+}
+
+double get_dp( double yp , double ym ){
+    double dy = yp-ym;
+    while( dy < 0.0 ) dp += y_max;
+    while( dy > y_max) dp -= y_max;
+    return(dy);
+}
+
+double get_signed_dp( double yp , double ym ){
+    double dy = yp-ym;
+    while( dy <-.5*y_max ) dp += y_max;
+    while( dy > .5*y_max ) dp -= y_max;
+    return(dy);
+}
+
+double get_centroid(double xp, double xm, int dim)
+{
+    return 0.5*(xp+xm);
+}
+
+double get_dL( double * xp , double * xm , int dim ){
+    if( dim==0 )
+    {
+        double dy = get_dp(xp[1], xm[1]);
+        return dy;
+    }
+    else if( dim==1 )
+        return xp[0]-xm[0];
+    else 
+        return xp[2]-xm[2];
+}
+
+double get_dA( double * xp , double * xm , int dim ){
+    double dx = xp[0]-xm[0];
+    double dy = get_dp(xp[1], xm[1]);
+    double dz = xp[2]-xm[2];
+    if( dim==0 )
+        return dx*dz;
+    else if( dim==1 )
+        return dy*dz;
+    else
+        return dx*dy;
+}
+
+double get_dV( double * xp , double * xm ){
+    double dx = xp[0]-xm[0];
+    double dy = get_dp(xp[1], xm[1]);
+    double dz = xp[2]-xm[2];
+    return dx*dy*dz;
+}
+
+double get_scale_factor( double * x, int dim)
+{
+    return 1.0;
+}
+
+double get_vol_element(double *x)
+{
+    return 1.0;
+}
+
+void get_xyz(double *x, double *xyz)
+{
+    xyz[0] = x[0];
+    xyz[1] = x[1];
+    xyz[2] = x[2];
+}
+
+void get_rpz(double *x, double *rpz)
+{
+    rpz[0] = sqrt(x[0]*x[0]+x[1]*x[1]);
+    rpz[1] = atan2(x[1]/x[0]);
+    rpz[2] = x[2];
+}
+
+void get_coords_from_xyz(double *xyz, double *x)
+{
+    x[0] = xyz[0];
+    x[1] = xyz[1];
+    x[2] = xyz[2];
+}
+
+void get_coords_from_rpz(double *rpz, double *x)
+{
+    x[0] = rpz[0] * cos(rpz[1]);
+    x[1] = rpz[0] * sin(rpz[1]);
+    x[2] = rpz[2];
+}
+
+void get_vec_rpz(double *x, double *v, double *vrpz)
+{
+    double r = sqrt(x[0]*x[0]+x[1]*x[1]);
+    double cp = x[0]/r;
+    double sp = x[1]/r;
+    vrpz[0] =  cp*v[0] + sp*v[1];
+    vrpz[1] = -sp*v[0] + cp*v[1];
+    vrpz[2] = v[2];
+}
+
+void get_vec_from_rpz(double *x, double *vrpz, double *v)
+{
+    double r = sqrt(x[0]*x[0]+x[1]*x[1]);
+    double cp = x[0]/r;
+    double sp = x[1]/r;
+    v[0] = cp*vrpz[0] - sp*vrpz[1];
+    v[1] = sp*vrpz[0] + cp*vrpz[1];
+    v[2] = vrpz[2];
+}
+
+void get_vec_xyz(double *x, double *v, double *vxyz)
+{
+    vxyz[0] = v[0];
+    vxyz[1] = v[1];
+    vxyz[2] = v[2];
+}
+void get_vec_from_xyz(double *x, double *vxyz, double *v)
+{
+    v[0] = vxyz[0];
+    v[1] = vxyz[1];
+    v[2] = vxyz[2];
+}
---
+++ Hydro/euler_cart.c
@@ -0,0 +1,238 @@
+
+#include "../paul.h"
+
+double get_om( double *);
+double get_om1( double *);
+double get_cs2( double *);
+
+static double gamma_law = 0.0; 
+static double RHO_FLOOR = 0.0; 
+static double PRE_FLOOR = 0.0; 
+static double explicit_viscosity = 0.0;
+static int include_viscosity = 0;
+static int isothermal = 0;
+static int alpha_flag = 0;
+static int polar_sources = 0;
+
+void setHydroParams( struct domain * theDomain ){
+   gamma_law = theDomain->theParList.Adiabatic_Index;
+   isothermal = theDomain->theParList.isothermal_flag;
+   RHO_FLOOR = theDomain->theParList.Density_Floor;
+   PRE_FLOOR = theDomain->theParList.Pressure_Floor;
+   explicit_viscosity = theDomain->theParList.viscosity;
+   include_viscosity = theDomain->theParList.visc_flag;
+   alpha_flag = theDomain->theParList.alpha_flag;
+}
+
+int set_B_flag(void){
+   return(0);
+}
+
+double get_omega( double * prim , double * x ){
+   return( prim[UPP] );
+}
+
+void planetaryForce( struct planet * , int , double , double , double * , double * );
+
+void prim2cons( double * prim , double * cons , double * x , double dV ){
+
+   double rho = prim[RHO];
+   double Pp  = prim[PPP];
+   double vr  = prim[URR];
+   double vp  = prim[UPP];
+   double vz  = prim[UZZ];
+   double om  = get_om( x );
+   double vp_off = vp - om;
+
+   double v2  = vr*vr + vp_off*vp_off + vz*vz;
+
+   double rhoe = Pp/(gamma_law - 1.);
+
+   cons[DDD] = rho*dV;
+   cons[TAU] = (.5*rho*v2 + rhoe )*dV;
+   cons[SRR] = rho*vr*dV;
+   cons[LLL] = rho*vp*dV;
+   cons[SZZ] = rho*vz*dV;
+
+   int q;
+   for( q=NUM_C ; q<NUM_Q ; ++q ){
+      cons[q] = prim[q]*cons[DDD];
+   }
+}
+
+void getUstar( double * prim , double * Ustar , double * x , double Sk , double Ss , double * n , double * Bpack ){
+
+   double rho = prim[RHO];
+   double vr  = prim[URR];
+   double vp  = prim[UPP];
+   double vz  = prim[UZZ];
+   double Pp  = prim[PPP];
+
+   double om = get_om( x );
+   double vp_off = vp - om;
+   double v2 = vr*vr+vp_off*vp_off+vz*vz;
+
+   double vn = vr*n[0] + vp*n[1] + vz*n[2];
+
+   double vn_off = vn - om*r*n[1];
+   double Ss_off = Ss + vn_off - vn;
+
+   double rhoe = Pp/(gamma_law - 1.);
+
+   double rhostar = rho*(Sk - vn)/(Sk - Ss);
+   double Pstar = Pp*(Ss - vn)/(Sk - Ss);
+   double Us = rhoe*(Sk - vn)/(Sk - Ss);
+
+   Ustar[DDD] = rhostar;
+   Ustar[SRR] = rhostar*( vr + (Ss-vn)*n[0] );
+   Ustar[LLL] = rhostar*( vp + (Ss-vn)*n[1] );
+   Ustar[SZZ] = rhostar*( vz + (Ss-vn)*n[2] );
+   Ustar[TAU] = .5*rhostar*v2 + Us + rhostar*Ss_off*(Ss - vn) + Pstar;
+
+   int q;
+   for( q=NUM_C ; q<NUM_Q ; ++q ){
+      Ustar[q] = prim[q]*Ustar[DDD];
+   }
+
+}
+
+void cons2prim( double * cons , double * prim , double * x , double dV ){
+   
+   double rho = cons[DDD]/dV;
+   if( rho < RHO_FLOOR )   rho = RHO_FLOOR;
+   double Sr  = cons[SRR]/dV;
+   double Sp  = cons[LLL]/dV;
+   double Sz  = cons[SZZ]/dV;
+   double E   = cons[TAU]/dV;
+   double om  = get_om( x );
+   
+   double vr = Sr/rho;
+   double vp = Sp/rho;
+   double vp_off = vp - om;
+   double vz = Sz/rho;
+
+   double KE = .5*( Sr*vr + rho*vp_off*vp_off + Sz*vz );
+   double rhoe = E-KE;
+   double Pp = (gamma_law - 1.)*rhoe;
+
+   if( Pp  < PRE_FLOOR*rho ) Pp = PRE_FLOOR*rho;
+   if( isothermal ){
+      double cs2 = get_cs2( x );
+      Pp = cs2*rho/gamma_law;
+   }
+
+   prim[RHO] = rho;
+   prim[PPP] = Pp;
+   prim[URR] = vr;
+   prim[UPP] = vp;
+   prim[UZZ] = vz;
+
+   int q;
+   for( q=NUM_C ; q<NUM_Q ; ++q ){
+      prim[q] = cons[q]/cons[DDD];
+   }
+
+}
+
+void flux( double * prim , double * flux , double * x , double * n ){
+   
+   double rho = prim[RHO];
+   double Pp  = prim[PPP];
+   double vr  = prim[URR];
+   double vp  = prim[UPP];
+   double vz  = prim[UZZ];
+   double om  = get_om( x );
+
+   double vn = vr*n[0] + vp*n[1] + vz*n[2];
+   double wn = om*n[1];
+   double vp_off = vp - om*r;
+
+   double rhoe = Pp/(gamma_law - 1.);
+   double v2 = vr*vr + vp_off*vp_off + vz*vz;
+
+   flux[DDD] = rho*vn;
+   flux[SRR] = rho*vr*vn + Pp*n[0];
+   flux[LLL] = rho*vp*vn + Pp*n[1];
+   flux[SZZ] = rho*vz*vn + Pp*n[2];
+   flux[TAU] = ( .5*rho*v2 + rhoe + Pp )*vn - Pp*wn;
+
+   int q;
+   for( q=NUM_C ; q<NUM_Q ; ++q ){
+      flux[q] = prim[q]*flux[DDD];
+   }
+   
+}
+
+double get_dp( double , double );
+double get_centroid( double , double , int);
+
+void source( double * prim , double * cons , double * xp , double * xm , double dVdt ){}
+
+void visc_flux( double * prim , double * gprim , double * flux , double * x , double * n ){}
+
+void flux_to_E( double * Flux , double * Ustr , double * x , double * E1_riemann , double * B1_riemann , double * E2_riemann , double * B2_riemann , int dim ){
+
+   //Silence is Golden.
+
+}
+
+void vel( double * prim1 , double * prim2 , double * Sl , double * Sr , double * Ss , double * n , double * x , double * Bpack ){
+
+   double P1   = prim1[PPP];
+   double rho1 = prim1[RHO];
+   double vn1  = prim1[URR]*n[0] + prim1[UPP]*n[1] + prim1[UZZ]*n[2];
+
+   double cs1 = sqrt(gamma_law*P1/rho1);
+
+   double P2   = prim2[PPP];
+   double rho2 = prim2[RHO];
+   double vn2  = prim2[URR]*n[0] + prim2[UPP]*n[1] + prim2[UZZ]*n[2];
+
+   double cs2 = sqrt(gamma_law*P2/rho2);
+
+   *Ss = ( P2 - P1 + rho1*vn1*(-cs1) - rho2*vn2*cs2 )/( rho1*(-cs1) - rho2*cs2 );
+
+   *Sr =  cs1 + vn1;
+   *Sl = -cs1 + vn1;
+
+   if( *Sr <  cs2 + vn2 ) *Sr =  cs2 + vn2;
+   if( *Sl > -cs2 + vn2 ) *Sl = -cs2 + vn2;
+
+}
+
+double get_dL( double * , double * , int );
+
+double mindt(double * prim , double w , double * xp , double * xm ){
+
+   double Pp  = prim[PPP];
+   double rho = prim[RHO];
+   double vp  = (prim[UPP]-w);
+   double vr  = prim[URR];
+   double vz  = prim[UZZ];
+   double cs  = sqrt(gamma_law*Pp/rho);
+
+   double maxvr = cs + fabs(vr);
+   double maxvp = cs + fabs(vp);
+   double maxvz = cs + fabs(vz);
+
+   double dtr = get_dL(xp,xm,1)/maxvr;
+   double dtp = get_dL(xp,xm,0)/maxvp;
+   double dtz = get_dL(xp,xm,2)/maxvz;
+   
+   double dt = dtr;
+   if( dt > dtp ) dt = dtp;
+   if( dt > dtz ) dt = dtz;
+
+   return( dt );
+}
+
+void reflect_prims(double * prim, double * x, int dim)
+{
+    //dim == 0: r, dim == 1: p, dim == 2: z
+    if(dim == 0)
+        prim[URR] = -prim[URR];
+    else if(dim == 1)
+        prim[UPP] = -prim[UPP];
+    else if(dim == 2)
+        prim[UZZ] = -prim[UZZ];
+}
