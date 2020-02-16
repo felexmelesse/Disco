806115d4f5b2328abe4d29d0511f419a20401665
ajdittmann <dittmann@astro.umd.edu>
2020-02-16 00:48:47 -0600

    .

--- Initial/kepler_pert.c
+++ Initial/kepler_pert.c
@@ -15,21 +15,18 @@ void initial( double * prim , double * x ){
 
    double r = x[0];
 
-   double rho = 1.0;
-   double Pp = rho/Mach/Mach/r;
+   double rho = 1.0 + 0.2*exp(-(r-15.0)*(r-15.0));
+   double Pp = rho*gam/Mach/Mach;
    double omega02 = 1.0/pow(r,3.);
    double omegaP2 = 0.0;
 
    double omega = sqrt( omega02 - omegaP2 );
 
    double X = 0.0; 
-   if( r*cos(x[1]) > 0.0 ) X = 1.0; 
 
    prim[RHO] = rho;
    prim[PPP] = Pp;
    prim[URR] = -1.5*nu/r;
    prim[UPP] = omega;
    prim[UZZ] = 0.0;
-   if( NUM_N>0 ) prim[NUM_C] = X;
-
 }
