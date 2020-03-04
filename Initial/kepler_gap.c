3d9c190c1a7e99be3c99ca2c01a3588681c628a5
Zeeve Rogoszinski <zrogoszinski@gmail.com>
2020-03-04 14:58:40 -0500

    changes to kepler_gap

--- Initial/kepler_gap.c
+++ Initial/kepler_gap.c
@@ -7,6 +7,8 @@ static double Mach = 0.0;
 static double q_planet	= 0.0;
 static double a		= 1.0;
 static double rot_om	= 0.0;
+static double mach_csd	= 0.0;
+static double alpha_visc= 0.0;
 
 void setICparams( struct domain * theDomain ){
    gam  = theDomain->theParList.Adiabatic_Index;
@@ -14,6 +16,8 @@ void setICparams( struct domain * theDomain ){
    Mach = theDomain->theParList.Disk_Mach;
    q_planet	= theDomain->theParList.Mass_Ratio;
    rot_om	= theDomain->theParList.RotOmega;
+   mach_csd	= theDomain->theParList.initPar1;
+   alpha_csd	= theDomain->theParList.initPar2; 
 }
 
 void initial( double * prim , double * x ){
@@ -27,12 +31,15 @@ void initial( double * prim , double * x ){
    double mu		= q_planet/(1.+q_planet);
    double omega		= sqrt(1./(R*R*R));
 
-   double alpha_visc	= nu*Mach*Mach/(a*a*omega);
-   double K		= q_planet*q_planet*Mach*Mach*Mach*Mach*Mach/alpha_visc;
-   
+   //double alpha_visc	= nu*Mach*Mach/(a*a*omega);
+   //double K		= q_planet*q_planet*Mach*Mach*Mach*Mach*Mach/alpha_visc;
+   double K		= pow(q_planet, 2)*pow(mach_csd, 5)/alpha_csd;
+
    double f_0		= 0.45;
-   double tau_sh	= 1.89 + 0.53/(q_planet*Mach*Mach*Mach);
-   double tau_r		= 0.3363585661*pow(fabs(1.5*Mach*((R/a) - 1.0)), 2.5);
+   //double tau_sh	= 1.89 + 0.53/(q_planet*Mach*Mach*Mach);
+   //double tau_r		= 0.3363585661*pow(fabs(1.5*Mach*((R/a) - 1.0)), 2.5);
+   double tau_sh	= 1.89 + 0.53/(q_planet*pow(mach_csd, 3));
+   double tau_r		= 0.3363585661*pow(fabs(1.5*mach_csd*((R/a) - 1.0)), 2.5);
    double f_r;
    if (tau_r < tau_sh){
 	f_r	= f_0;
@@ -47,6 +54,7 @@ void initial( double * prim , double * x ){
    if(q_planet <= 0.0)
        rho = rho_0;
    double Pp		= rho/(gam*Mach*Mach);
+   //double Pp		= rho/(gam*mach_csd*mach_csd);
 
    double X = 0.0; 
    if( r*cos(x[1]) > 0.0 ) X = 1.0; 
@@ -56,19 +64,21 @@ void initial( double * prim , double * x ){
    double dr_dPhi	= -a*sin(phi);
    double dphi_dPhi	= 1.0 - (a/r)*cos(phi);
 
-   double vr_global	= dr_dPhi*(sqrt(1./R) - sqrt(1./a));
+   double vr_global	= dr_dPhi*(sqrt(1./(R*R*R)) - sqrt(1./(a*a*a)));
    double vp_global	= dphi_dPhi*(sqrt(1./(R*R*R)) - sqrt(1./(a*a*a))) + rot_om;
    double vp_local	= sqrt(mu/(r*r*r));
    
    //Fixing angular velocity profile 
    double r_in		= 0.005;
    double r_out		= 0.01;
-   double vr_f, vp_f, slope_p, slope_r;
+   double vr_f, vp_f, slope_p, slope_r, slope_rho;
+   double rho_in	= 0.1*rho_0;
    
    if (r < r_in)
    {
 	vp_f	= vp_local;
 	vr_f	= 0;
+	rho	= rho_in;
    }
    else if (r_in < r && r < r_out)
    {
@@ -77,9 +87,13 @@ void initial( double * prim , double * x ){
 
 	slope_r	= (vr_global - 0)/(r_out - r_in);
 	vr_f	= 0 + slope_r*(r-r_in);
+	
+	slope_rho = (rho - rho_in)/(r_out - r_in);
+	rho	= rho_in + slope_rho*(r-r_in);
    }else{
 	vp_f	= vp_global;
 	vr_f	= vr_global;
+	rho	= rho;
    }
 
    prim[RHO] = rho;
