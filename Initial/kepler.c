00857f8fc529aaaf429425bcb2fc67dd12028121
Alexander Dittmann <dittmann@astro.umd.edu>
2020-03-28 19:33:51 -0400

    softening scripts

--- Initial/kepler.c
+++ Initial/kepler.c
@@ -21,10 +21,12 @@ void initial( double * prim , double * x ){
    double omega02 = 1.0/pow(R,3.);
    double omegaP2 = (1.5/(Mach*Mach*R*R*R)) - (2.25*nu*nu/(Mach*Mach*Mach*Mach*r*r))*(0.5/R - R/(r*r));
 
-   double omega = sqrt( omega02 - omegaP2 );
+   double omega2 = fmax( (omega02 - omegaP2), 0.0 );
+   double omega = sqrt(omega2);
    double cs2 = 1.0/(R*Mach*Mach);
-   double visc = nu*sqrt(R)/(Mach*Mach);
-   double rho = 1.0/visc;
+   double visc = nu*sqrt(r)/(Mach*Mach);
+   double rho = 1.0;
+   if (nu > 0) rho = 1.0/visc;
    double Pp = rho*cs2;
 
    double X = 0.0; 
@@ -32,7 +34,7 @@ void initial( double * prim , double * x ){
 
    prim[RHO] = rho;
    prim[PPP] = Pp;
-   prim[URR] = -1.5*visc/r;
+   prim[URR] = -1.5*visc/(r);
    prim[UPP] = omega;
    prim[UZZ] = 0.0;
    if( NUM_N>0 ) prim[NUM_C] = X;
