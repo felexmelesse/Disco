37549a3f0ddda7e014caa1313cdd48faa7f50d5d
Alexander Dittmann <dittmann@astro.umd.edu>
2020-12-11 13:17:16 -0500

    Added preliminary boundary layer diagnostic.

---
+++ Diagnostics/diag_bl.c
@@ -0,0 +1,42 @@
+
+#include "../paul.h"
+
+static double gamma_law = 0.0;
+
+void setDiagParams( struct domain * theDomain ){
+   gamma_law = theDomain->theParList.Adiabatic_Index;
+}
+
+int num_diagnostics(void){
+   return(84);
+}
+
+/* Generic Diagnostics for 2D boundary layers. Only good for m<40 modes.*/
+
+void get_diagnostics( double * x , double * prim , double * Qrz, 
+                        struct domain * theDomain )
+{
+   double r = x[0];
+   double phi = x[1];
+   double z = x[2];
+
+   double rho = prim[RHO];
+   double vr = prim[URR];
+   double omega = prim[UPP];
+   double vz = prim[UZZ];
+   double Pp = prim[PPP];
+   double vp = r*omega;
+
+   Qrz[0] = rho;
+   Qrz[1] = rho*vp;
+   Qrz[2] = rho*vr;
+   Qrz[3] = rho*vp*vr;
+
+   int i;
+   double val = r*vr*sqrt(rho)/M_PI;
+   for (i=0; i<40; i++) {
+      Qrz[i*2+4] = val*cos(phi*(i+1));
+      Qrz[i*2+5] = val*sin(phi*(i+1));
+   }
+}
+
