86047667d9f8dfc2337594f2dcc2d8567aa3524a
Alexander Dittmann <40439219+ajdittmann@users.noreply.github.com>
2020-12-17 16:25:47 -0500

    Update diag_bl.c
    Remove Fourier modes from time average.

--- Diagnostics/diag_bl.c
+++ Diagnostics/diag_bl.c
@@ -8,7 +8,8 @@ void setDiagParams( struct domain * theDomain ){
 }
 
 int num_diagnostics(void){
-   return(84);
+   //return(85);
+   return(5);
 }
 
 /* Generic Diagnostics for 2D boundary layers. Only good for m<40 modes.*/
@@ -28,15 +29,16 @@ void get_diagnostics( double * x , double * prim , double * Qrz,
    double vp = r*omega;
 
    Qrz[0] = rho;
-   Qrz[1] = rho*vp;
-   Qrz[2] = rho*vr;
-   Qrz[3] = rho*vp*vr;
+   Qrz[1] = rho*omega;
+   Qrz[2] = rho*vp;
+   Qrz[3] = rho*vr;
+   Qrz[4] = rho*vp*vr;
 
-   int i;
-   double val = r*vr*sqrt(rho)/M_PI;
-   for (i=0; i<40; i++) {
-      Qrz[i*2+4] = val*cos(phi*(i+1));
-      Qrz[i*2+5] = val*sin(phi*(i+1));
-   }
+   //int i;
+   //double val = r*vr*sqrt(rho)/M_PI;
+   //for (i=0; i<40; i++) {
+   //   Qrz[i*2+4] = val*cos(phi*(i+1));
+   //   Qrz[i*2+5] = val*sin(phi*(i+1));
+   //}
 }
 
