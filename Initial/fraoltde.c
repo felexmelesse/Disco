8f6d616d9d8a98486bdffb3ac90191ed6a9b9871
Fraol Kebede <fraolmelesse83@gmail.com>
2021-07-08 15:23:21 -0400

    Edited fraoltde for the orbit properties

--- Initial/fraoltde.c
+++ Initial/fraoltde.c
@@ -1,8 +1,10 @@
 
 #include "../paul.h"
 static const double Gm_s = 1;
+static double r_out = 1;
 
 void setICparams( struct domain * theDomain ){
+   r_out = theDomain->theParList.rmax;
 }
 
 void initial( double * prim , double * x ){
@@ -14,35 +16,32 @@ void initial( double * prim , double * x ){
    double X = 0.0;
 //if( cos(phi) > 0.0 ) X = 1.0;
 
-   double omega, disk, angular_v, radial_v, E, J, rho, p, vr, r_out, E_mb, a_mb ,e_mb, M;
-   double w, R0, l0,rho0, rhoatm, patm, l, e, phip, phi0, rp, ra, a, M_star, R_star, beta, R_T;
-   E = -0.02;
-   J = 3;
+   double omega, angular_v, radial_v, E, J, rho, p, vr, E_mb, a_mb ,e_mb, M;
+   double w, R0, rho0, rhoatm, patm, l, e, phip, phi0, rp, a, M_star, R_star, beta, R_T;
+   //E = -0.02;
+   //J = 3;
    M_star = 1.0e-6;
    R_star = 0.47;
    beta = 5;
    M = 1;
-   rp = 4;
-   ra = 20;
-   r_out =10;
+   //rp = 4;
+   //ra = 20;
    w = 0.01;
-   l0 = 0.8;
+   //l0 = 0.8;
    patm = 1.0e-5;
    rhoatm = 1.0e-3;
    rho0 = 1 - rhoatm;
 
-   R_T = beta*rp;
+   R_T = R_star*pow(M/M_star,1.0/3.0);
+   rp = R_T/beta;
    E_mb = -M*R_star/(R_T*R_T);
    a_mb = (R_T*R_T)/2*R_star;
-   e_mb = (1-rp)/a_mb;
-   a = (ra + rp)/2;
-//   l = (J*J)/(Gm_s);
-   e = (ra-rp)/(ra+rp);
-//   e = sqrt(1+((2*J*J*E)/(Gm_s*Gm_s)));
+   e_mb = 1-(rp/a_mb);
+   a = a_mb;
+   e = e_mb;
    l = a*(1-(e*e));
-   //e = 0.5;
    phip = M_PI;
-   //l = r*(1+e*cos(phi-phip));
+   
 
    J = sqrt(Gm_s*l);
    E = -0.5*(1-(e*e))*(Gm_s/l);
@@ -57,7 +56,7 @@ void initial( double * prim , double * x ){
    
    radial_v = ((r*r)/l)*(e*sin(phi-phip))*angular_v;
    
-   disk = rhoatm + (rho0*exp(-((l-l0)*(l-l0))/(2*w*w)));
+//   disk = rhoatm + (rho0*exp(-((l-l0)*(l-l0))/(2*w*w)));
    //printf("phip %f, phi %f, phi0 %f\n",phip, phi, phi0);
    
    if ((r > r_out) && (phi0 - 5*w < phi) && (phi < phi0 + 5*w)){
