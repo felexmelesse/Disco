3b270fb2172330642be9274033c5b0d70e06a5a8
Fraol Kebede <fkebede@yorp09.node>
2021-07-09 11:27:32 -0400

    Added new boundery condetion

---
+++ Boundary/fixed_accrete.c
@@ -0,0 +1,23 @@
+
+#include "../paul.h"
+#include "../boundary.h"
+
+void boundary_trans( struct domain * theDomain , int dim )
+{
+    int nq = 1;
+    int q[1];
+    q[0] = UPP;
+
+    if(dim == 1)
+    {
+        boundary_zerograd_rinn(theDomain, 1);
+        boundary_fixed_q_rinn(theDomain, q, nq);
+        boundary_fixed_rout(theDomain);
+    }
+    else if(dim == 2)
+    {
+        boundary_zerograd_zbot(theDomain, 1);
+        boundary_zerograd_ztop(theDomain, 1);
+    }
+}
+
