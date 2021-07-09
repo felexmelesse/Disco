abb8ed289211371f9c538e1bde781af6b61bfa0e
Fraol Kebede <fkebede@yorp09.node>
2021-07-09 11:36:24 -0400

    Added fixed_q header to the boundry.h file

--- boundary.h
+++ boundary.h
@@ -15,6 +15,11 @@ void boundary_fixed_rout( struct domain *theDomain);
 void boundary_fixed_zbot( struct domain *theDomain);
 void boundary_fixed_ztop( struct domain *theDomain);
 
+void boundary_fixed_q_rinn( struct domain *theDomain, int *q, int nq);
+void boundary_fixed_q_rout( struct domain *theDomain, int *q, int nq);
+void boundary_fixed_q_zbot( struct domain *theDomain, int *q, int nq);
+void boundary_fixed_q_ztop( struct domain *theDomain, int *q, int nq);
+
 void boundary_zerograd_rinn( struct domain *theDomain, int diode);
 void boundary_zerograd_rout( struct domain *theDomain, int diode);
 void boundary_zerograd_zbot( struct domain *theDomain, int diode);
@@ -44,9 +49,9 @@ void boundary_noslip_ztop( struct domain *theDomain);
 //Low-level routines for boundary
 void set_cell_init(struct cell *c, const double *r_jph, const double *z_kph,
                    int j, int k);
-void set_cell_init_q(struct cell *c, const double *r_jph, const double *z_kph, 
+void set_cell_init_q(struct cell *c, const double *r_jph, const double *z_kph,
                      int j, int k, int *qarr, int nq);
-void set_cells_copy(struct cell *c, int Np, const struct face *theFaces, 
+void set_cells_copy(struct cell *c, int Np, const struct face *theFaces,
                     int n0, int n1, int LR);
 void set_cells_copy_distant(struct cell *c, int Np, struct cell *c1, int Np1);
 
