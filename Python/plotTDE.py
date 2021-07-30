8efe03cb2796aa309bcf4eb57208c00e3a8cc6ab
Fraol Kebede <fraolmelesse83@gmail.com>
2021-07-30 17:30:14 -0400

    Edited the pythinTDE to add a new e vs r cut plot

--- Python/plotTDE.py
+++ Python/plotTDE.py
@@ -9,6 +9,17 @@ import matplotlib.pyplot as plt
 import discopy.util as util
 import discopy.plot as plot
 import discopy.geom as geom
+from matplotlib import cm
+from matplotlib.colors import Normalize as norm
+
+C = 299792458.0
+G = 6.674e-11
+M_solar = 1.989e30
+M_BH = 1e6*M_solar
+tg_sec = G*M_BH/(C*C*C)
+Rg_meter = C*tg_sec
+Rg_AU = 6.6846e-12*Rg_meter
+t_disk = 1e4
 
 def analyzecheCkpoint(file):
     
@@ -73,46 +84,82 @@ def analyzecheCkpoint(file):
 
 if __name__=="__main__":
 
+
     fig_e, ax_e = plt.subplots(1,1, figsize=(12,9))
     fig_rho, ax_rho = plt.subplots(1,1, figsize=(12,9))
     fig_H, ax_H = plt.subplots(1,1, figsize=(12,9))
+    fig_e_cut, ax_e_cut = plt.subplots(1,1, figsize=(12,9))
     cmap = plt.get_cmap("plasma")
-
+    ts = []
     for i, f in enumerate(sys.argv[1:]):
 
         t, R, ecc_r,rho_r, HovrR_r= analyzecheCkpoint(Path(f))
-        label = "time = %.0f" % t
+        ts.append(t*tg_sec/(3600))
+        label = "time = %.0f" % (t*tg_sec/(3600))
         color = cmap(i/(len(sys.argv[1:])-1))
-        ax_e.plot(R,ecc_r,label = label, color = color)
-        ax_rho.plot(R,rho_r,label = label, color = color)
-        ax_H.plot(R,HovrR_r,label = label, color = color)
-
-    ax_e.legend()
+        ax_e.plot(R*Rg_AU,ecc_r,label = label, color = color)
+        ax_rho.plot(R*Rg_AU,rho_r,label = label, color = color)
+        ax_H.plot(R*Rg_AU,HovrR_r,label = label, color = color)
+        if t > t_disk: 
+            ax_e_cut.plot(R*Rg_AU,ecc_r,label = label, color = color)
+    
+    fig_e.subplots_adjust(top = 0.95, bottom=0.05, left = 0.1, right = 0.8)
+    cax = fig_e.add_axes([0.85,0.05,0.05, 0.9])
+    cbar = plt.colorbar(cm.ScalarMappable(norm = norm(0.0, vmax = np.max(ts)), cmap=plt.get_cmap('plasma')), cax = cax)
+    cbar.set_label("Time (Hr)")
+   
+    #ax_e.legend()
     ax_e.set_ylim(0,1.5)
-    ax_e.set_xlabel("R")
+    ax_e.set_xlabel("R (AU)")
     ax_e.set_ylabel("e")
     figname = "r_vs_e_time.png"
     print("saving",figname)
     fig_e.savefig(figname)
     plt.close(fig_e)
 
-    ax_rho.legend()
-    ax_rho.set_xlabel("R")
+    fig_rho.subplots_adjust(top = 0.95, bottom=0.05, left = 0.1, right = 0.8)
+    cax = fig_rho.add_axes([0.85,0.05,0.05, 0.9])
+    cbar = plt.colorbar(cm.ScalarMappable(norm = norm(0.0, vmax = np.max(ts)), cmap=plt.get_cmap('plasma')), cax = cax)
+    cbar.set_label("Time (Hr)")
+   
+    
+    #ax_rho.legend()
+    ax_rho.set_xlabel("R (AU)")
     ax_rho.set_ylabel(r"$\rho$")
     figname = "r_vs_rho_time.png"
     print("saving",figname)
     fig_rho.savefig(figname)
     plt.close(fig_rho)
 
-    ax_H.legend()
+    fig_H.subplots_adjust(top = 0.95, bottom=0.05, left = 0.1, right = 0.8)
+    cax = fig_H.add_axes([0.85,0.05,0.05, 0.9])
+    cbar = plt.colorbar(cm.ScalarMappable(norm = norm(0.0, vmax = np.max(ts)), cmap=plt.get_cmap('plasma')), cax = cax)
+    cbar.set_label("Time (Hr)")
+   
+    
+    #ax_H.legend()
     ax_H.set_ylim(0,2)
-    ax_H.set_xlabel("R")
+    ax_H.set_xlabel("R (AU)")
     ax_H.set_ylabel("H/R")
     figname = "r_vs_HovrR_time.png"
     print("saving",figname)
     fig_H.savefig(figname)
     plt.close(fig_H)
 
+    fig_e_cut.subplots_adjust(top = 0.95, bottom=0.05, left = 0.1, right = 0.8)
+    cax = fig_e_cut.add_axes([0.85,0.05,0.05, 0.9])
+    cbar = plt.colorbar(cm.ScalarMappable(norm = norm(0.0, vmax = np.max(ts)), cmap=plt.get_cmap('plasma')), cax = cax)
+    cbar.set_label("Time (Hr)")
+   
+    #ax_e.legend()
+    ax_e_cut.set_xlim(0,2)
+    ax_e_cut.set_ylim(0.8,1.1)
+    ax_e_cut.set_xlabel("R (AU)")
+    ax_e_cut.set_ylabel("e")
+    figname = "r_vs_e_cut_time.png"
+    print("saving",figname)
+    fig_e_cut.savefig(figname)
+    plt.close(fig_e_cut)
 
 
 
